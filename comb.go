81c828fc0eaefb563b565a6217505ba7a7ba3466
Ben Levy <bjlgds@gmail.com>
2014-01-18 16:29:10 -0500

    added verbosity based logging messages

--- comb.go
+++ comb.go
@@ -1,10 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 func Choose(a, b int) int {
 	if a-b > b {
 		b = a - b
 	}
 	var r = Perm(a, b) / Fact(a-b)
+	if Verbosity >= Combinatorics_Verbosity {
+		fmt.Printf("%d C %d = %d\n", a, b, r)
+	}
 	return r
 }
 
@@ -12,6 +19,9 @@ func Perm(a, b int) int {
 	var r int = 1
 	for x := a; x > b; x-- {
 		r *= x
+	}
+	if Verbosity >= Combinatorics_Verbosity {
+		fmt.Printf("%d P %d = %d\n", a, b, r)
 	}
 	return r
 }
@@ -21,29 +31,8 @@ func Fact(a int) int {
 	for x := int(2); x <= a; x++ {
 		r *= x
 	}
-	return r
-}
-
-func ChooseB(a, b int64) int64 {
-	if a-b > b {
-		b = a - b
-	}
-	var r = PermB(a, b) / FactB(a-b)
-	return r
-}
-
-func PermB(a, b int64) int64 {
-	r := int64(1)
-	for x := a; x > b; x-- {
-		r *= x
-	}
-	return r
-}
-
-func FactB(a int64) int64 {
-	r := int64(1)
-	for x := int64(2); x <= a; x++ {
-		r *= x
+	if Verbosity >= Combinatorics_Verbosity {
+		fmt.Printf("%d! = %d\n", a, r)
 	}
 	return r
 }
--- prob.go
+++ prob.go
@@ -2,3 +2,15 @@ package main
 
 type probability float64
 type space_t float64
+
+var (
+	Verbosity = 201
+)
+
+//Verbosity levels
+const (
+	Low_Verbosity           = 0
+	Med_Verbosity           = 100
+	High_Verbosity          = 200
+	Combinatorics_Verbosity = 201
+)
