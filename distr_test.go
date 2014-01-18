758acc527295cdc6c4abd5449097d469f4f297d2
Ben Levy <bjlgds@gmail.com>
2014-01-18 16:36:34 -0500

    more tests

--- distr_test.go
+++ distr_test.go
@@ -6,11 +6,23 @@ import (
 )
 
 func Test_Distributions(t *testing.T) {
+
 	a := makeBernoulliDistribution(.5)
-	b := makeBinomialDistribution(.5, 8)
-	c := makeUniformDistribution(6)
-	d := makeHyperGeomDistribution(5, 5, 3)
-	e := makePoissonDistribution(.5)
+	b := makeBernoulliDistribution(.001)
+
+	//For large values of the second value the probability stops summing to 1
+	c := makeBinomialDistribution(.5, 8)
+	d := makeBinomialDistribution(.1, 10)
+
+	e := makeUniformDistribution(6)
+	f := makeUniformDistribution(1000)
+
+	g := makeHyperGeomDistribution(5, 5, 3)
+	h := makeHyperGeomDistribution(17, 10, 6)
+
+	i := makePoissonDistribution(.9)
+	j := makePoissonDistribution(.5)
+	k := makePoissonDistribution(.1)
 
 	Convey("Probability should always sum to around 1", t, func() {
 
@@ -19,5 +31,11 @@ func Test_Distributions(t *testing.T) {
 		So(1.0, ShouldAlmostEqual, c.sumProbs())
 		So(1.0, ShouldAlmostEqual, d.sumProbs())
 		So(1.0, ShouldAlmostEqual, e.sumProbs())
+		So(1.0, ShouldAlmostEqual, f.sumProbs())
+		So(1.0, ShouldAlmostEqual, g.sumProbs())
+		So(1.0, ShouldAlmostEqual, h.sumProbs())
+		So(1.0, ShouldAlmostEqual, i.sumProbs())
+		So(1.0, ShouldAlmostEqual, j.sumProbs())
+		So(1.0, ShouldAlmostEqual, k.sumProbs())
 	})
 }
